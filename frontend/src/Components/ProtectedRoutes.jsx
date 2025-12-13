866b6a1ed8cafe04c60732ac878ee4dc009dc05d
Kush Patel <kushpatelj@Mac.lan>
2025-12-13 11:10:42 -0800

    added a about page

--- frontend/src/Components/ProtectedRoutes.jsx
+++ frontend/src/Components/ProtectedRoutes.jsx
@@ -2,5 +2,11 @@ import {Outlet, Navigate} from "react-router-dom";
 
 export function ProtectedRoutes (){
     const user =null;
-    return user ? <Outlet /> : <Navigate to="/login"/>
+    if (user) {
+        return <Outlet />;
+    }
+    else
+    {
+        return <Navigate to="/login" />;
+    }
 }
---
+++ frontend/src/Pages/About.jsx
@@ -0,0 +1,18 @@
+import './Styles/About.css'
+
+
+
+export function About(){
+    return (
+
+        <div id='about'>
+            <p>
+           Kush In Tech Chat is a chatting system built with Node.js and WebSockets. 
+           It supports user authentication with strong password requirements, message encryption,
+           rate limiting, and limited logging attempts. It uses a secure WebSocket connection (wss://)
+            over HTTPS. This chat applications gives users option to send it to everyone or individuals
+            </p>
+        </div>
+        
+    )
+}
